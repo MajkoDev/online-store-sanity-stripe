40aa682ca9927d32be108a74d8b05cd495116a6b
Marian <hellomariandev@gmail.com>
2024-01-10 11:33:03 +0100

    sanity

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "skTR1Dnxj5G7uvx8jT81UevWMW2nVMw2P7ea7b1xFdhvQWB2u7QCmz9d2ZC7DkLmjUkp39MJdAFjugxvL2cu5FiY6F3MAzJ8OdjxWrhYD5KiUClkFJORmPrkzch5pbLPboNUQlPHZU9G9y2m467cpVjUQX5GYbBEPe5Mi77eTQykH9z9sLrF"
 })
