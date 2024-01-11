0cb5bac3ccad705c6312be576a7672bb19e733f6
Marian <hellomariandev@gmail.com>
2024-01-11 09:34:15 +0100

    stripe checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,4 +5,35 @@ import { validateCartItems } from "use-shopping-cart/utilities"
 import { inventory } from "@/config/inventory"
 import { stripe } from "@/lib/stripe"
 
-export async function POST() {}
+export async function POST(request: Request) {
+  const cartDetails = await request.json()
+  const lineItems = validateCartItems(inventory, cartDetails)
+  const origin = request.headers.get("origin")
+
+  const session = await stripe.checkout.sessions.create({
+    submit_type: "pay",
+    mode: "payment",
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    shipping_address_collection: {
+      allowed_countries: ["US"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: 'Fixed Amount',
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 450,
+            currency: "usd",
+          },
+        },
+      },
+    ],
+    billing_address_collection: "auto",
+    success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${origin}/cart`,
+  })
+
+  return NextResponse.json(session)
+}
